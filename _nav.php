09a9f756ba111bf1a959c48e4547329947f9fd1e
jian-hao <h804880521@gmail.com>
2021-06-04 20:43:41 +0800

    nav and professor page
    1.new nav
    2.upload professor introduction and paper

--- _nav.php
+++ _nav.php
@@ -1,12 +1,12 @@
-<ul class="nav nav-fill">
+<ul class="nav nav-tabs" id="nav-tab" role="tablist"style="font-size:24px;">
 	<li class="nav-item">
-		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=professor" ?> >教授簡介</a>
+		<a class="nav-link active" href=<?php echo "{$_SERVER['PHP_SELF']}?page=professor" ?> >教授簡介</a>
 	</li>
 	<li class="nav-item">
 		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=research" ?> >研究領域</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=member&class=MD" ?> >實驗室成員</a>
+		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=member" ?> >實驗室成員</a>
 	</li>
 	<li class="nav-item">
 		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=instrument" ?> >設備儀器</a>
