d2c2958d3aba89a7bf52dc971f88a286d58beff7
jian-hao <h804880521@gmail.com>
2021-06-05 15:22:45 +0800

    edit nav
    1.new nav type
    2.make auto active

--- _nav.php
+++ _nav.php
@@ -1,20 +1,32 @@
-<ul class="nav nav-tabs" id="nav-tab" role="tablist"style="font-size:24px;">
-	<li class="nav-item">
-		<a class="nav-link active" href=<?php echo "{$_SERVER['PHP_SELF']}?page=professor" ?> >教授簡介</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=research" ?> >研究領域</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=member" ?> >實驗室成員</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=instrument" ?> >設備儀器</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href=<?php echo "{$_SERVER['PHP_SELF']}?page=life" ?> >生活集錦</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="http://www.cme.ncu.edu.tw/">回化材系</a>
-	</li>
-</ul>
+<?php
+	if(isset($_GET['page']) AND !empty($_GET['page'])){
+	    $page = $_GET['page'];
+	}else{
+	    $page = "home";
+	}
+	$pageList = array(
+					array('professor', '教授簡介'),
+					array('research', '研究領域'),
+					array('member', '實驗室成員'),
+					array('instrument', '設備儀器'),
+					array('life', '生活集錦'));
+
+	echo '<ul class="nav nav-tabs" id="nav-tab" role="tablist"style="font-size:24px;">';
+
+	for ( $i=0 ; $i<5 ; $i++ ) {
+		echo '<li class="nav-item">';
+
+		if ($pageList[$i][0]==$page) {
+			echo '<a class="nav-link active" href='.$_SERVER['PHP_SELF'].'?page='.$pageList[$i][0].'>'.$pageList[$i][1].'</a>';
+		}
+		else{
+			echo '<a class="nav-link" href='.$_SERVER['PHP_SELF'].'?page='.$pageList[$i][0].'>'.$pageList[$i][1].'</a>';
+		}
+
+		echo '</li>';
+	}
+	
+	echo '</ul>';
+
+
+?>
