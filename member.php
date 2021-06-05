0662884ced2ceb8265b418f9b25badac3d3d9f74
jian-hao <h804880521@gmail.com>
2021-06-05 15:44:21 +0800

    change member page Special effects
    1.home: new build
    2.stu and gra: change pic Flip mode

--- member.php
+++ member.php
@@ -1,3 +1,4 @@
+
 <?php
 
 	if(isset($_GET['class']) AND !empty($_GET['class'])){
@@ -19,3 +20,5 @@
 	}
 
 ?>
+
+
--- member_home.php
+++ member_home.php
@@ -1,5 +1,7 @@
-
+<!--
+<img src="images/choose.png" style="width: 1560px;"/> 
 	<div class="choose">
+
 		<a href=<?php echo "{$_SERVER['PHP_SELF']}?page=member&class=student" ?>>
 			<img src="images/orange.png"/> 
 		</a>
@@ -7,3 +9,70 @@
 			<img src="images/green.png"/>
 		</a>
 	</div>
+-->
+<div style="background-image:url(images/choose.png); background-size:1560px; height:1000px">
+	<div class="main">			
+		<div class="card">
+			<div class="cardTop">
+				<a href=<?php echo "{$_SERVER['PHP_SELF']}?page=member&class=student" ?>>	
+					<div class="viewImg">
+						<img src="https://www.jq22.com/img/cs/500x500-2.png" alt="">
+					</div>
+				</a>
+				<div class="viewText">
+					<h3>
+						在校生<br>
+						<span>Electronic Thin Film LAB</span>
+					</h3>
+
+				</div>
+			</div>
+			<!--
+			<ul class="cardBtm">
+				<li type="--i:1">
+					<a href="###">
+						<i class="fa fa-weixin" aria-hidden="true">碩士班</i>
+					</a>
+				</li>
+				<li style="--i:2">
+					<a href="###">
+						<i class="fa fa-weibo" aria-hidden="true">博士班</i>
+					</a>
+				</li>
+			</ul>-->
+		</div>
+		
+		
+		<div class="card">
+			<div class="cardTop">
+				<a href=<?php echo "{$_SERVER['PHP_SELF']}?page=member&class=graduate" ?>>
+					<div class="viewImg">
+						<img src="https://www.jq22.com/img/cs/500x500-2.png" alt="">
+					</div>
+				</a>
+				<div class="viewText">
+					<h3>
+						畢業生<br>
+						<span>Electronic Thin Film LAB</span>
+					</h3>
+
+				</div>
+			</div>
+			<!--
+			<ul class="cardBtm">
+				<li type="--i:1">
+					<a href="###">
+						<i class="fa fa-weixin" aria-hidden="true">碩士班</i>
+					</a>
+				</li>
+				<li style="--i:2">
+					<a href="###">
+						<i class="fa fa-weibo" aria-hidden="true">博士班</i>
+					</a>
+				</li>
+			</ul>
+		-->
+		</div>
+		
+	</div>
+</div>
