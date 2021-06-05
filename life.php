958d7fba9f2541c8b51dacc0fb1af4243e0cdcb0
jian-hao <h804880521@gmail.com>
2021-06-05 19:51:48 +0800

    build life page
    life page done

--- life.php
+++ life.php
@@ -1,9 +1,59 @@
 
-<div class="container" style="height:800px; background-color:#FFFFFF; margin-top:50px; padding-left:50px;padding-top:20px;">
-	<div style="text-align:center">
-		<span style="font-family:DFKai-sb;font-weight:bold;font-size:36px;">生活集錦</span>
-	</div>
-	<div>
-		<img src="images/life/2009.jpg" class="d-block w-100">
-	</div>
+
+<!-- Slideshow container -->
+<div class="slideshow-container">
+	<?php 
+		$memberData = array('2009','2010','2012','2013','2014','2015','2016','2017');
+		for ( $j=0 ; $j<count($memberData) ; $j++ ) {
+			echo '<div class="mySlides">';
+			echo '<img src="images/life/'.$memberData[$j].'.jpg">';
+			echo '</div>';
+		}
+
+	?>
+	<!-- Next and previous buttons -->
+	<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+	<a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
+	<br>
+
+	<!-- The dots/circles -->
+<div style="text-align:center">
+	<span class="dot act" onclick="currentSlide(1)"></span>
+	<span class="dot" onclick="currentSlide(2)"></span>
+	<span class="dot" onclick="currentSlide(3)"></span>
+	<span class="dot" onclick="currentSlide(4)"></span>
+	<span class="dot" onclick="currentSlide(5)"></span>
+	<span class="dot" onclick="currentSlide(6)"></span>
+	<span class="dot" onclick="currentSlide(7)"></span>
+	<span class="dot" onclick="currentSlide(8)"></span>
+</div>
+
+<script>
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  var dots = document.getElementsByClassName("dot");
+	  if (n > slides.length) {slideIndex = 1}    
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";  
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	      dots[i].className = dots[i].className.replace(" act", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";  
+	  dots[slideIndex-1].className += " act";
+	}
+</script>
