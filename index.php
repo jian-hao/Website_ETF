e73a597ed401ebe2bb16a4e50e518a32f987f922
jian-hao <h804880521@gmail.com>
2021-06-13 12:43:29 +0800

    fix life page bug

--- index.php
+++ index.php
@@ -14,8 +14,20 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="css/hao.css">
 
-		<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+		<!-- <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script> -->
 		<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.js"></script>
+
+
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/css/swiper.css" rel="stylesheet" />
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/css/swiper.min.css" rel="stylesheet" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/js/swiper.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/js/swiper.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/life.css">
+		
+		<script type="text/javascript" async="" src="js/life.js"></script>
+		
+
+
 
 		<link rel="icon" href="images/favicon.ico">
 		<title>電子薄膜實驗室</title>
@@ -29,7 +41,7 @@
 		</div>
 		<div class="row">
 			<div class="container-hao">
-				<div class="content">			
+				<div class="ETF_Main_content">			
 					<?php 
 						include('_breadcrumb.php');
 						switch($page){  
--- life.php
+++ life.php
@@ -1,59 +1,81 @@
+<?php
+	$con = mysqli_connect("localhost", "jianhao", "pass574f5244", "armydb");
+	$sql = 'SELECT * FROM `upload_data`';
+	$rows = mysqli_query($con, $sql); 
 
 
-<!-- Slideshow container -->
-<div class="slideshow-container">
-	<?php 
-		$memberData = array('2009','2010','2012','2013','2014','2015','2016','2017');
-		for ( $j=0 ; $j<count($memberData) ; $j++ ) {
-			echo '<div class="mySlides">';
-			echo '<img src="images/life/'.$memberData[$j].'.jpg">';
-			echo '</div>';
-		}
+	$photo = array(
+					array('2014', '實驗室出遊', '20140813_實驗室出遊_1.jpg'),
+					array('2014', '實驗室出遊', '20140813_實驗室出遊_2.jpg'),
+					array('2014', '實驗室出遊', '20140813_實驗室出遊_3.jpg'),
+					array('2014', '實驗室出遊', '20140813_實驗室出遊_4.jpg'),
+					array('2014', '實驗室出遊', '20140813_實驗室出遊_6.jpg'),
+					array('2014', '實驗室出遊', '20140813_實驗室出遊_8.jpg'),
+					array('2014', '實驗室出遊', '20140813_實驗室出遊_9.jpg'),
 
-	?>
-	<!-- Next and previous buttons -->
-	<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-	<a class="next" onclick="plusSlides(1)">&#10095;</a>
+					array('2015', '淡江遊', '20150117_淡江遊_0.jpg'),
+					array('2015', '淡江遊', '20150117_淡江遊_1.jpg'),
+					array('2015', '淡江遊', '20150117_淡江遊_2.jpg'),
+					array('2015', '淡江遊', '20150117_淡江遊_3.jpg'),
+					array('2015', '淡江遊', '20150117_淡江遊_4.jpg'),
+
+					array('2016', '臺東出遊', '2016_臺東實驗室出遊_1.jpg'),
+					array('2016', '臺東出遊', '2016_臺東實驗室出遊_2.jpg'),
+					array('2016', '臺東出遊', '2016_臺東實驗室出遊_3.jpg'),
+					array('2016', '臺東出遊', '2016_臺東實驗室出遊_4.jpg'),
+					array('2016', '臺東出遊', '2016_臺東實驗室出遊_5.jpg'),
+					array('2016', '臺東出遊', '2016_臺東實驗室出遊_6.jpg'),
+					array('2016', '臺東出遊', '2016_臺東實驗室出遊_7.jpg'),
+
+					array('2020', '基隆瑞芳遊', '2020_實驗室出遊_基隆瑞芳_1.jpg'),
+					array('2020', '基隆瑞芳遊', '2020_實驗室出遊_基隆瑞芳_2.jpg'),
+					array('2020', '基隆瑞芳遊', '2020_實驗室出遊_基隆瑞芳_3.jpg'),
+					array('2020', '基隆瑞芳遊', '2020_實驗室出遊_基隆瑞芳_4.jpg'),
+					array('2020', '基隆瑞芳遊', '2020_實驗室出遊_基隆瑞芳_5.jpg'),
+					array('2020', '基隆瑞芳遊', '2020_實驗室出遊_基隆瑞芳_7.jpg'));
+
+
+?>
+
+<div class="offset-2" style="height: 570px;">
+	<div class="swiper-container main-slider loading">
+		<div class="swiper-wrapper">
+			<?PHP
+				for ($i=0; $i<count($photo); $i++) {
+					
+					echo '<div class="swiper-slide">';
+						echo '<figure class="slide-bgimg" style="background-image:url(images/life/travel/'.$photo[$i][2].')">';
+							echo '<img src="images/life/travel/'.$photo[$i][2].'" class="entity-img" />';
+						echo '</figure>';
+						echo '<div class="content">';
+							echo '<p class="title">'.$photo[$i][0].'</p>';
+							echo '<span class="caption">'.$photo[$i][1].'</span>';
+						echo '</div>';
+					echo '</div>';
+				}
+			?>
+
+		</div>
+		<div class="swiper-button-prev swiper-button-white"></div>
+		<div class="swiper-button-next swiper-button-white"></div>
+	</div>
+
+	<div class="swiper-container nav-slider loading">
+		<div class="swiper-wrapper" role="navigation">
+			<?PHP
+				for ($i=0; $i<count($photo); $i++) {
+					$bug_i = $i+4;
+					if ($bug_i>=count($photo)){$bug_i=$bug_i-count($photo);}
+					echo '<div class="swiper-slide">';
+						echo '<figure class="slide-bgimg" style="background-image:url(images/life/travel/'.$photo[$bug_i][2].')">';
+							echo '<img src="images/life/travel/'.$photo[$bug_i][2].'" class="entity-img" />';
+						echo '</figure>';
+						echo '<div class="content">';
+							echo '<p class="title">'.$photo[$bug_i][0].'</p>';
+						echo '</div>';
+					echo '</div>';
+				}
+			?>
+		</div>
+	</div>
 </div>
-	<br>
-
-	<!-- The dots/circles -->
-<div style="text-align:center">
-	<span class="dot act" onclick="currentSlide(1)"></span>
-	<span class="dot" onclick="currentSlide(2)"></span>
-	<span class="dot" onclick="currentSlide(3)"></span>
-	<span class="dot" onclick="currentSlide(4)"></span>
-	<span class="dot" onclick="currentSlide(5)"></span>
-	<span class="dot" onclick="currentSlide(6)"></span>
-	<span class="dot" onclick="currentSlide(7)"></span>
-	<span class="dot" onclick="currentSlide(8)"></span>
-</div>
-
-<script>
-	var slideIndex = 1;
-	showSlides(slideIndex);
-
-	function plusSlides(n) {
-	  showSlides(slideIndex += n);
-	}
-
-	function currentSlide(n) {
-	  showSlides(slideIndex = n);
-	}
-
-	function showSlides(n) {
-	  var i;
-	  var slides = document.getElementsByClassName("mySlides");
-	  var dots = document.getElementsByClassName("dot");
-	  if (n > slides.length) {slideIndex = 1}    
-	  if (n < 1) {slideIndex = slides.length}
-	  for (i = 0; i < slides.length; i++) {
-	      slides[i].style.display = "none";  
-	  }
-	  for (i = 0; i < dots.length; i++) {
-	      dots[i].className = dots[i].className.replace(" act", "");
-	  }
-	  slides[slideIndex-1].style.display = "block";  
-	  dots[slideIndex-1].className += " act";
-	}
-</script>
